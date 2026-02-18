2cbebd82b66b458cfb6ea5182f0c375103bdf7cb
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2026-02-18 22:36:54 +0530

    feat: Initialize AI chatbot project with chat API, UI components, and AI SDK integration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["imgbb.com", "i.ibb.co"],
+  },
 };
 
 export default nextConfig;
